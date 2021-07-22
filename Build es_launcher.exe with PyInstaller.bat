fdec03603cf76ee05ebf20930c5a183b30fd1a83
JungleJango <87671634+JungleJango@users.noreply.github.com>
2021-07-22 17:37:51 -0300

    Add files via upload

---
+++ Build es_launcher.exe with PyInstaller.bat
@@ -0,0 +1,11 @@
+pip install --upgrade pyinstaller
+RMDIR /S /Q "__pycache__"
+RMDIR /S /Q "build"
+RMDIR /S /Q "dist"
+pyinstaller -w -F "es_launcher.pyw" -i "icon.ico"
+del "es_launcher.spec"
+RMDIR /S /Q "__pycache__"
+RMDIR /S /Q "build"
+move ".\dist\es_launcher.exe" "."
+RMDIR /S /Q "dist"
+pause
